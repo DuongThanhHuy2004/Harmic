43723a4b84e8fe66c066d3e1f54e96686960ca3b
DuongThanhHuy2004 <thanhhuy22102004@gmail.com>
2024-12-01 18:29:22 +0700

    sua

--- obj/Debug/net9.0/Harmic.AssemblyInfo.cs
+++ obj/Debug/net9.0/Harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e409225e1baaf70d1612e3b9b7bc6143370509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babed1bd122ea5c83b2eb63a76553ac13f2ba640")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
